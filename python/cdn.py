c79159d397930b738a1c4edec3c4a509fc3dedab
pnemski <pbnhous3@abv.bg>
2022-12-04 05:41:19 -0500

    python make automatization

---
+++ python/cdn.py
@@ -0,0 +1,5 @@
+x=float (input("enter x:\n"))
+y=float (input("enter y:\n"))
+z= x-y
+print (z)
+
---
+++ python/makeauto.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+echo "run python if"
+make run1 
+echo "run cdn"
+make run2
+echo "finish"
+
