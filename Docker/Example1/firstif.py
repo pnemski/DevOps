ff218154af1a16bb0c9655cf75cae12310bed7a7
pnemski <pnemski@yahoo.com>
2022-12-21 17:47:55 +0200

    test

--- Docker/Example1/firstif.py
+++ Docker/Example1/firstif.py
@@ -1,9 +1,9 @@
-x= int (input("Enter x\n"))
-y= int (input("Enter y\n"))
-z= 10
+x= int (input("Enter x\n"));
+y= int (input("Enter y\n"));
+z= 10;
 if z>x and z<y:
     print ("true")
 else: 
-    print ("false")
-print ("finish\n")
+    print ("false");
+print ("finish\n");
 
